d8c91a63717b0c753ab30854288dbf236a89608d
AlexandrPavlushenko <a.s.pavlushenko@gmail.com>
2024-12-04 01:25:37 +0300

    stage2

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls', namespace='users')),
     path('', include('catalog.urls', namespace='catalog')),
     path('blog/', include('blog.urls', namespace='blog'))
 ]
